8c2b2b74129aa57564ac1f2fb643457985ee2a54
Damian Ly <dsly@uci.edu>
2019-05-13 12:12:47 -0700

    first commit

---
+++ Project3/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project3.c
+ *
+ * Created: 5/13/2019 12:05:41 PM
+ * Author : lydam
+ */ 
+
+#include <avr.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
